6b8226f47fea47bd80568b9a2f6fda3099445f26
Zachary Leeper <zachl@cs.utexas.edu>
2022-10-14 21:17:54 -0500

    Updated setup.sh with the password

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 user="" # Your username here
 remote="" # Your section's machine here
 
+# The password for ac_lab_host is T2CnUMhNwG8T2M3
+# Use this ONLY for the setup portion.
+
 echo "SSHing to the remote VM..."
 ssh -i Test_key.pem ac_lab_host@$remote sudo ./setup_user.sh $user
 echo "\nSetup complete!"
